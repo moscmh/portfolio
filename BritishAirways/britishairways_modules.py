75bbd18d7fa3365790868ccbf3705a9b0a76423a
Man Ho Cheung <mos.cheung.mh@gmail.com>
2025-08-04 00:04:42 +0800

    Add files via upload

---
+++ BritishAirways/britishairways_modules.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.style.use('tableau-colorblind10') # mpl.style.available
+import seaborn as sns
+sns.set_style("white")
